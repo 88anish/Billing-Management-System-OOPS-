2ca7507c3f231d9cddc42d2ac667b1149efda16e
Anish Jain <87357590+88anish@users.noreply.github.com>
2023-07-16 10:43:05 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,230 +1,140 @@
-#include<iostream>  
-#include<process.h>  
-#include<fstream>  
-#include <stdlib.h>  
-using namespace std;  
-class head  
-{  
-    char Iname[50][50];  
-  
-public:  
-    int totalitems;  
-    float Qty[3];  
-    float price[3];  
-    int vatprice[3];  
-    int tprice[3];  
-    void input();  
-    void output();  
-};  
-class vat:public head  
-{  
-    float vats;  
-public:  
-    void vatcal();  
-    void outputs();  
-    void sum();  
-};  
-  
-//******************************************************************  
-//      INPUT FUNCTION  
-//******************************************************************  
-  
-void head::input()  
-{  
-    system("CLS");  
-    cout<<"\nEnter number of items= ";  
-    cin>>totalitems;  
-  
-    for(int i=0; i<totalitems; i++)  
-    {  
-        cout<<"\nEnter name of item "<<i+1<<": ";  
-        cin>>Iname[i];  
-        cout<<"Enter quantity: ";  
-        cin>>Qty[i];  
-        cout<<"Enter price of item "<<i+1<<": ";  
-        cin>>price[i];  
-        tprice[i]=Qty[i]*price[i];  
-    }  
-}  
-//* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *   
-//     OUTPUT FUNCTION  
-//* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *   
-void head::output()  
-{  
-    int a;  
-  
-    ifstream infile("COUNT.TXT");  
-    infile>>a;  
-  
-    ofstream outfile("COUNT.TXT");  
-    a+=1;  
-    outfile<<a;  
-    outfile.close();  
-  
-    {ofstream outfile("HIS.TXT", ios::app);  
-    outfile<<endl<<"Bill No.: "<<a<<endl;  
-    outfile<<"------------------------------------------------------------------------"<<endl;  
-cout<<"\n";  
-    cout<<"Name of Item\tQuantity   Price  Total Price\n";  
-    for(int i=0;i<totalitems;i++)  
-    {  
-        outfile<<"Name: "<<Iname[i]<<" Qty: "<<Qty[i]<<" Price: "<<tprice[i]<<endl;  
-        cout<<Iname[i]<<"\t\t"<<Qty[i]<<"\t   "<<price[i]<<"\t   "<<tprice[i]<<'\n';  
-    }  
-  
-    outfile<<"------------------------------------------------------------------------"<<endl;  
-    outfile.close();  
-    }  
-}  
-//* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *   
-//     VAT CALCULATION  
-//* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *   
-void vat::vatcal()  
-{  
-    input();  
-    for(int i=0;i<totalitems;i++)  
-    {  
-        if(price[i]<=100.00)  
-        {  
-            vatprice[i]=tprice[i]+(0.03*tprice[i]);  
-        }  
-        else  
-        {  
-            vatprice[i]=tprice[i]+(0.1*tprice[i]);  
-        }  
-    }  
-}  
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *   
-//      VAT OUTPUTS  
-//*  * * * * * * * * * * * * * * * * * * * * ** * * * * * * * * * * * * * * * * * * * * * * * * *    
-void vat::outputs()  
-{  
-    output();  
-  
-    float cash=0,sum=0,qty=0,sumt=0;  
-  
-    for(int i=0;i<totalitems;i++)  
-    {  
-           sumt+=tprice[i];  
-           sum+=vatprice[i];  
-           qty+=Qty[i];  
-    }  
-    cout<<"\nTotal:";  
-    cout<<"\n------------------------------------------------------------------------------";  
-    cout<<"\n\tQuantity= "<<qty<<"\t\t Sum= "<<sumt<<"\tWith Vat:"<<sum;  
-    cout<<"\n------------------------------------------------------------------------------";  
-  
-pay:  
-  
-    cout<<"\n\n\t\t\t * * * * PAYMENT SUMMARY * * * * \n";  
-    cout<<"\n\t\t\tTotal cash given: ";  
-    cin>>cash;  
-  
-    if(cash>=sum)  
-        cout<<"\n\t\t\tTotal cash repaid: "<<cash-sum<<'\n';  
-          
-    else  
-    {   cout<<"\n\t\t\tCash given is less than total amount!!!";  
-  
-    goto pay;  
-    }  
-}  
-//* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *   
-//      PROTECTION PASSWORD  
-//* * * * * * * * * * * *  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *   
-  
-int passwords()  
-{  
-  
-    char p1,p2,p3;  
-  
-    cout<<"\n\n\n\n\n\n\t\t\tENTER THE PASSWORD: ";  
-  
-    cin>>p1;  
-    cout<<"*";  
-    cin>>p2;  
-    cout<<"*";  
-    cin>>p3;  
-    cout<<"*";  
-  
-    if ((p1=='s'||p1=='S')&&(p2=='i'||p2=='I')&&(p3=='d'||p3=='D'))  
-  
-        return 1;  
-  
-    else  
-        return 0;  
-}  
-// END of Password.  
-  
-//****************************************************************  
-//      THE MAIN FUNCTION OF PROGRAM  
-//****************************************************************  
-  
-  
-int main()  
-{  
-    vat obj;  
-    char opt, ch;  
-    int a=1;  
-    ifstream fin;  
-  
-    a==passwords();  
-    if(!a)  
-    {  
-        for(int i=0;i<2;i++)  
-        {  
-            cout<<"\nWrong password try once more\n";  
-            if(passwords())  
-            {  
-                goto last;  
-            }  
-            else  
-            {  
-                cout<<"\n\n\t\t\t all attempts failed.....";  
-                cout<<"\n\n\n\t\t\t see you.................. ";  
-                exit(0);  
-            }  
-  
-        }  
-        cout<<"\t\t\t sorry all attempts failed............. \n \t\t\tinactive";  
-             }  
-    else{  
-last:;   
-     do{  
-start:  
-    system("PAUSE");  
-    system("CLS");  
-    cout<<"\n\n\t\t\t------------------------------";  
-    cout<<"\n\t\t\tBilling Management System";  
-    cout<<"\n\t\t\t------------------------------";  
-     cout<<"\n\n\t\t\tWhat you want to do?";  
-     cout<<"\n\t\t\t1.\tTo enter new entry\n\t\t\t2.\tTo view previous entries\n\t\t\t3.\tExit\n";  
-     cout<<"\n\nEnter your option: ";  
-     cin>>opt;  
-     switch(opt)  
-     {  
-     case'1':  
-         obj.vatcal();  
-  
-         obj.outputs();  
-         goto start;  
-     case'2':  
-  
-         fin.open("HIS.TXT", ios::in);  
-         while(fin.get(ch))  
-         {  
-             cout<<ch;  
-         }  
-         fin.close();  
-  
-         goto start;  
-     case'3':  
-         exit(0);  
-     default:  
-         cout<<"\a";  
-     }  
-  
-     }while(opt!=3);  
-    }  
-    return 0;  
-}  
+#include <iostream>
+#include <vector>
+#include <set>
+
+class Student {
+private:
+    std::string firstName;
+    std::string lastName;
+    int rollNumber;
+    float cgpa;
+    std::vector<int> courseIDs;
+
+public:
+    // Constructor
+    Student(const std::string& fName, const std::string& lName, int roll, float gpa, const std::vector<int>& cIDs)
+        : firstName(fName), lastName(lName), rollNumber(roll), cgpa(gpa), courseIDs(cIDs) {}
+
+    // Getter functions
+    std::string getFirstName() const { return firstName; }
+    std::string getLastName() const { return lastName; }
+    int getRollNumber() const { return rollNumber; }
+    float getCGPA() const { return cgpa; }
+    std::vector<int> getCourseIDs() const { return courseIDs; }
+};
+
+class StudentManagementSystem {
+private:
+    std::vector<Student> students;
+    std::set<int> rollNumbers;
+
+public:
+    // Function to add a student
+    bool addStudent(const Student& student) {
+        if (rollNumbers.count(student.getRollNumber()) == 0) {
+            students.push_back(student);
+            rollNumbers.insert(student.getRollNumber());
+            return true;
+        } else {
+            return false; // Roll number already exists
+        }
+    }
+
+    // Function to find a student by roll number
+    Student findStudentByRollNumber(int roll) const {
+        for (const Student& student : students) {
+            if (student.getRollNumber() == roll) {
+                return student;
+            }
+        }
+        // Return an empty student object if not found
+        return Student("", "", -1, -1.0, {});
+    }
+
+    // ... Implement other functions for finding, deleting, updating students ...
+
+    // Function to print the total number of students
+    void printTotalStudents() const {
+        std::cout << "Total number of students: " << students.size() << std::endl;
+    }
+};
+
+int main() {
+    StudentManagementSystem sms;
+
+    int numStudents;
+    std::cout << "Enter the number of students to add: ";
+    std::cin >> numStudents;
+
+    for (int i = 0; i < numStudents; i++) {
+        std::cout << "Student " << i + 1 << ":\n";
+        
+        std::string firstName, lastName;
+        int rollNumber;
+        float cgpa;
+        std::vector<int> courseIDs;
+
+        std::cout << "Enter First Name: ";
+        std::cin >> firstName;
+        std::cout << "Enter Last Name: ";
+        std::cin >> lastName;
+        
+        bool isUniqueRoll = false;
+        while (!isUniqueRoll) {
+            std::cout << "Enter Roll Number: ";
+            std::cin >> rollNumber;
+            
+            if (rollNumber < 0 || sms.findStudentByRollNumber(rollNumber).getRollNumber() != -1) {
+                std::cout << "Invalid or duplicate Roll Number. Please enter a unique positive integer." << std::endl;
+            } else {
+                isUniqueRoll = true;
+            }
+        }
+        
+        std::cout << "Enter CGPA: ";
+        std::cin >> cgpa;
+
+        int numCourses;
+        std::cout << "Enter the number of courses: ";
+        std::cin >> numCourses;
+        std::cout << "Enter the Course IDs:\n";
+        for (int j = 0; j < numCourses; j++) {
+            int courseID;
+            std::cin >> courseID;
+            courseIDs.push_back(courseID);
+        }
+
+        Student student(firstName, lastName, rollNumber, cgpa, courseIDs);
+        bool added = sms.addStudent(student);
+        if (added) {
+            std::cout << "Student added successfully.\n";
+        } else {
+            std::cout << "Failed to add student. Roll Number already exists.\n";
+            i--; // Retry entering details for the same student
+        }
+    }
+
+    int rollNumber;
+    std::cout << "Enter the Roll Number to find a student: ";
+    std::cin >> rollNumber;
+
+    Student foundStudent = sms.findStudentByRollNumber(rollNumber);
+    if (foundStudent.getRollNumber() == -1) {
+        std::cout << "Student with roll number " << rollNumber << " not found." << std::endl;
+    } else {
+        std::cout << "Found student:\n";
+        std::cout << "First Name: " << foundStudent.getFirstName() << std::endl;
+        std::cout << "Last Name: " << foundStudent.getLastName() << std::endl;
+        std::cout << "Roll Number: " << foundStudent.getRollNumber() << std::endl;
+        std::cout << "CGPA: " << foundStudent.getCGPA() << std::endl;
+        std::cout << "Course IDs:";
+        for (int courseID : foundStudent.getCourseIDs()) {
+            std::cout << " " << courseID;
+        }
+        std::cout << std::endl;
+    }
+
+    sms.printTotalStudents();
+
+    return 0;
+}
